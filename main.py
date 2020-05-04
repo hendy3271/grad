ee94d80ed02e6eb1bd61e34a4c949cd98f9f8b34
hendy3271 <42560902+hendy3271@users.noreply.github.com>
2020-05-04 18:28:17 +1000

    example of using gradient

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from grad import Variable, trace
 
-a = Variable(10)
-b = Variable(2)
+a = Variable(12)
+b = Variable(3)
 c = 5
 d = c*a
-e = d*b
 
-trace(e, a)
+e = a+2+a+a+100
+
+grd = trace(e, a)
+print(grd)
