8fddef65ba031b8d08d0454c0948b257554d8176
hendy3271 <42560902+hendy3271@users.noreply.github.com>
2020-05-04 17:03:03 +1000

    Simpler main

--- main.py
+++ main.py
@@ -2,15 +2,8 @@ from grad import Variable, trace
 
 a = Variable(10)
 b = Variable(2)
+c = 5
+d = c*a
+e = d*b
 
-c = a*b
-
-c *= 5
-
-d = a * 5
-
-e = d*c
-f = c*d
-
-trace(e)
-trace(f)
+trace(e, a)
