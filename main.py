ee9f4f1779d6a96e2494a3fdfe8719a69d86b58d
hendy3271 <42560902+hendy3271@users.noreply.github.com>
2020-05-04 16:11:59 +1000

    Grad testing

---
+++ main.py
@@ -0,0 +1,45 @@
+from grad import Variable
+from typing import overload
+
+p = lambda a, b: print('a  = {a}, b  = {b}'.format(a=a, b=b))
+p_ = lambda a, b: print('a_ = {a}, b_ = {b}'.format(a=a, b=b))
+
+a = 10.
+a_ = Variable(10)
+b = a
+b_ = a_
+
+p(a, b)
+p_(a_, b_)
+assert a == a_, b == b_
+
+a = a + 1
+a_ = a_ + 1
+
+p(a, b)
+p_(a_, b_)
+assert a == a_, b == b_
+
+a += 1.
+a_ += 1.
+
+p(a, b)
+p_(a_, b_)
+assert a == a_, b == b_
+
+b = a*5
+b_ = a_*5
+
+p(a, b)
+p_(a_, b_)
+assert a == a_, b == b_
+
+def trace(variable):
+    while True:
+        print(variable , ' : ', variable.operation)
+        if variable.parent is None:
+            break
+        variable = variable.parent
+        
+
+trace(a_)
