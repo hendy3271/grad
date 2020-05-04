0b4625bf6c45cb8a179c9a95351797d4043e9e97
hendy3271 <42560902+hendy3271@users.noreply.github.com>
2020-05-04 18:39:00 +1000

    reduced to just grad function

--- main.py
+++ main.py
@@ -1,17 +1,8 @@
-from grad import Variable, trace, grad
-
-a = Variable(12)
-b = Variable(3)
-c = 5
-d = c*a
-
-e = a*a+2*a+4
+from grad import grad
 
 y = lambda x: x*x+2*x+4
 dydx = grad(y)
 
-grd = trace(e, a)
-print(e)
-print(grd)
-
-print(dydx(12))
+x = 3
+y_12, dydx_12 = dydx(x)
+print('y({x}) = {y}, dydx({x}) = {dydx}'.format(x=x, y=y_12, dydx=dydx_12))
