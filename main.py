f099f2259a7da0424b721f8e2ee7792a358d5c43
hendy3271 <42560902+hendy3271@users.noreply.github.com>
2020-05-04 20:09:03 +1000

    Added check in main

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 from grad import grad
 
-y = lambda x: 1/(x*x*x)
+y = lambda x: (x**2 + 5*x - 2)/(3**x)
 dydx = grad(y)
 
+h = 1.e-7
+dydx_ca = lambda x: (y(x+h) - y(x-h))/(2*h)
+
 x = 3
-h = 0.000001
+
 y_12, dydx_12 = dydx(x)
 
-a_dydx = (y(x+h) - y(x-h))/(2*h)
+a_dydx = dydx_ca(x)
 
 print('y({x}) = {y}, dydx({x}) = {dydx}'.format(x=x, y=y_12, dydx=dydx_12))
 print('dydx ~ {}'.format(a_dydx))
+error = abs(dydx_12-a_dydx)
+assert error < h
