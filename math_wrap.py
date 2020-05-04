81bfd0a580e24099b7d68c7862b593356655cd03
hendy3271 <42560902+hendy3271@users.noreply.github.com>
2020-05-04 23:09:24 +1000

    implemented logs derivatives

--- math_wrap.py
+++ math_wrap.py
@@ -1,4 +1,4 @@
-from math import sin, cos, sqrt, exp
+from math import sin, cos, sqrt, exp, log
 from grad import Variable, primitive, simple_primitive
 
 min_, max_ = min, max
@@ -14,3 +14,6 @@ exp = simple_primitive(exp_, [exp])
 
 sqrt_ = sqrt
 sqrt = simple_primitive(sqrt_, [lambda x: 0.5/sqrt(x)])
+
+log_ = log
+log = simple_primitive(log_, [lambda x, base=exp(1): 1/(log(base)*x), lambda x, base=exp(1): log(x)/(base*log(base)**2)])
