bf2e19b429b0692d93a932c4054c6c31809c6b9c
hendy3271 <42560902+hendy3271@users.noreply.github.com>
2020-05-04 18:33:20 +1000

    example of using grad function

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
-from grad import Variable, trace
+from grad import Variable, trace, grad
 
 a = Variable(12)
 b = Variable(3)
 c = 5
 d = c*a
 
-e = a+2+a+a+100
+e = a*a+2*a+4
+
+y = lambda x: x*x+2*x+4
+dydx = grad(y)
 
 grd = trace(e, a)
+print(e)
 print(grd)
+
+print(dydx(12))
