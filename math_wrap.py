012dfe523e43ef99da41968843cc89bd91ff434e
hendy3271 <42560902+hendy3271@users.noreply.github.com>
2020-05-04 22:35:33 +1000

    made derivatives differentiable

--- math_wrap.py
+++ math_wrap.py
@@ -6,11 +6,11 @@ min_, max_ = min, max
 sin_ = sin
 cos_ = cos
 
-sin = simple_primitive(sin_, [cos_])
-cos = simple_primitive(cos_, [lambda x: -sin_(x)])
+sin = simple_primitive(sin_, [cos])
+cos = simple_primitive(cos_, [lambda x: -sin(x)])
 
 exp_ = exp
-exp = simple_primitive(exp_, [exp_])
+exp = simple_primitive(exp_, [exp])
 
 sqrt_ = sqrt
-sqrt = simple_primitive(sqrt_, [lambda x: 0.5/sqrt_(x)])
+sqrt = simple_primitive(sqrt_, [lambda x: 0.5/sqrt(x)])
