018b4f486ae605b99dbcfc7d7f8c8ffd912cbb50
hendy3271 <42560902+hendy3271@users.noreply.github.com>
2020-05-04 16:12:06 +1000

    grad working

---
+++ grad.py
@@ -0,0 +1,64 @@
+def grad(func, arg=0):
+    def dfunc(*args, **kwargs):
+        if isinstance(arg, (int, float)):
+            # wrap args[arg]
+            args=list(args)
+            var = Variable(args[arg])
+            args[arg] = var
+            pass 
+        elif isinstance(arg, str):
+            # wrap kwargs[arg]
+            var = Variable(args[arg])
+            kwargs[arg] = var
+            pass
+        else:
+            raise TypeError
+
+        # Foward pass
+        value = func(*args, **kwargs)
+
+        # Reverse pass
+        gradient = trace.reverse(value)
+
+        return value.get_value(), gradient
+    return dfunc
+
+def method_overload(method):
+    def new_method(self, *args, **kwargs):
+        x = getattr(self.super, method.__name__)(*args, **kwargs)
+
+        operation = method(self, x)
+        if operation:
+            return operation
+        return x
+
+    return new_method
+
+def operation_overload(method):
+    def new_method(self, *args, **kwargs):
+        x = getattr(self.super, method.__name__)(*args, **kwargs)
+
+        operation = method(self, x)
+        if operation:
+            return operation
+        return x
+
+    return new_method
+
+class Variable(float):
+    def __new__(self, *args, **kwargs):
+        return super().__new__(self, *args)
+
+    def __init__(self, *args, **kwargs):
+        self.parent = kwargs.pop('parent', None)
+        self.operation = kwargs.pop('operation', None)
+        self.super = super()
+        self.super.__init__()
+
+    @method_overload
+    def __add__(self, x):
+        return Variable(x, parent=self, operation='add')
+    
+    def __str__(self):
+        return 'V' + super().__str__()
+    pass
