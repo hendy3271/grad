81cb85aae421b89c600bfb60c4002324b48e5ca3
hendy3271 <42560902+hendy3271@users.noreply.github.com>
2020-05-04 18:59:52 +1000

    added approximation check

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 from grad import grad
 
-y = lambda x: x*x+2*x+4
+y = lambda x: 1/(x*x*x)
 dydx = grad(y)
 
 x = 3
+h = 0.000001
 y_12, dydx_12 = dydx(x)
+
+a_dydx = (y(x+h) - y(x-h))/(2*h)
+
 print('y({x}) = {y}, dydx({x}) = {dydx}'.format(x=x, y=y_12, dydx=dydx_12))
+print('dydx ~ {}'.format(a_dydx))
