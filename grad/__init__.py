3dfeefbc451ad2858a153224ee843475ad614a23
hendy3271 <42560902+hendy3271@users.noreply.github.com>
2020-05-05 14:53:11 +1000

    Added differentiate to init

--- grad/__init__.py
+++ grad/__init__.py
@@ -1,3 +1,3 @@
-from .grad import grad, primitive, simple_primitive
+from .grad import grad, primitive, simple_primitive, differentiate
 from .variables import Variable, operation_overload, method_overload
 from .tester import tester
