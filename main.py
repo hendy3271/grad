5847905bc9b4e2c39b7b75f3f2226c549f4c9fee
hendy3271 <42560902+hendy3271@users.noreply.github.com>
2020-05-05 10:52:46 +1000

    Added tests for new function derivatives

--- main.py
+++ main.py
@@ -1,41 +1,71 @@
 from tester import tester
 from grad import primitive
-from math_wrap import sin, cos, sqrt, exp, log
+from math_wrap import *
 
 @primitive([lambda *args: 2, lambda *args: -3])
 def var2(x, y):
      return 2.*x - 3.*y
 
 N = 3
+x = 2.
+x_2 = 0.5
 
 y = lambda x: x**2
-x = 2.
 tester(y, x, h=1.e-6, name='square', n=N)
 
 y = lambda x: abs(x)
-x = 2.
 tester(y, x, h=1.e-6, name='abs', n=N)
 
 y = lambda x: cos(x)
-x = 2.
 tester(y, x, h=1.e-6, name='cos', n=N)
 
 y = lambda x: sin(x)
-x = 2.
 tester(y, x, h=1.e-6, name='sin', n=N)
 
 y = lambda x: sqrt(x)
-x = 2.
 tester(y, x, h=1.e-6, name='sqrt', n=N)
 
 y = lambda x: log(x)
-x = 2.
 tester(y, x, h=1.e-6, name='log', n=N)
 
 y = lambda x: exp(x)
-x = 2.
 tester(y, x, h=1.e-6, name='exp', n=N)
 
+y = lambda x: asin(x)
+tester(y, x_2, h=1.e-6, name='asin', n=N)
+
+y = lambda x: acos(x)
+tester(y, x_2, h=1.e-6, name='acos', n=N)
+
+y = lambda x: atan(x)
+tester(y, x, h=1.e-6, name='atan', n=N)
+
+y = lambda x: atan2(x, 1)
+tester(y, x, h=1.e-6, name='atan2_0', n=N)
+
+y = lambda x: atan2(1, x)
+tester(y, x, h=1.e-6, name='atan2_1', n=N)
+
+y = lambda x: atan2(x, x)
+tester(y, x, h=1.e-6, name='atan2_2', n=N)
+
+y = lambda x: sinh(x)
+tester(y, x, h=1.e-6, name='sinh', n=N)
+
+y = lambda x: cosh(x)
+tester(y, x, h=1.e-6, name='cosh', n=N)
+
+y = lambda x: tanh(x)
+tester(y, x, h=1.e-6, name='tanh', n=N)
+
+y = lambda x: asinh(x)
+tester(y, x, h=1.e-6, name='asinh', n=N)
+
+y = lambda x: acosh(x)
+tester(y, x, h=1.e-6, name='acosh', n=N)
+
+y = lambda x: atanh(x)
+tester(y, x_2, h=1.e-6, name='atanh', n=N)
+
 y = lambda x: var2(x, x)
-x = 2.
 tester(y, x, h=1.e-6, name='user primitive', n=N)
