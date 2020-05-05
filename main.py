3202be1d5c35f99ebf0994e6e9cd9902c27228e2
hendy3271 <42560902+hendy3271@users.noreply.github.com>
2020-05-05 12:14:31 +1000

    updated vectorize

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from math import sin, pi
-from matplotlib.pyplot import plot, show
+from math import sin, pi, atan2
+from matplotlib.pyplot import plot, show, legend
 
 def linspace(start, stop, N = 50):
     step = (stop-start)/(N-1)
@@ -8,14 +8,46 @@ def linspace(start, stop, N = 50):
 
 x = list(linspace(-pi, pi))
 
+# need grad_vectorise
+# and vectorize grad
 def vectorize(func):
     def vfunc(*args):
+        maximum = 0.
+        for arg in args:
+            if isinstance(arg, (list, tuple)):
+                maximum = max(maximum, len(arg))
+
+        if maximum == 0.:
+            return func(*args)
+
+        sargs = []
+        for arg in args:
+            if isinstance(arg, (list, tuple)):
+                sargs.append(list(arg))
+            else:
+                from itertools import cycle
+                sargs.append(cycle([arg]))
+
         vector = []
-        for vargs in zip(*args):
+
+        for vargs in zip(*sargs):
             vector.append(func(*vargs))
         return vector
     return vfunc
 
+@vectorize
+def square(x):
+    return x**2
+
+@vectorize
+def relu(x):
+    if x < 0.:
+        return 0.
+    return x
+
 vsin = vectorize(sin)
-plot(x, vsin(x))
+
+plot(x, vsin(x), label = 'sin(x)')
+plot(x, square(relu(x)), label = 'square(relu(x))')
+legend()
 show()
