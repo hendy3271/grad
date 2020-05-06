dbb0baff6c5a603cd79a21de371ba3729a687ceb
hendy3271 <paul.g.hendy@gmail.com>
2020-05-06 16:47:35 +1000

    removed the need for extra functions in variable

--- grad/variables.py
+++ grad/variables.py
@@ -7,7 +7,7 @@ log = lambda x, *args: 0. if abs(x) < 1e-322 else log_(abs(x), *args)
 def operation_overload(method):
     def new_method(self, *args):     
         func = getattr(self.super, method.__name__)
-        gradients = getattr(self, method_diff(method.__name__))()
+        gradients = method(self, *args)
         return Variable(func(*args), parents=[self] + list(args), gradients=gradients)
     return new_method
 
@@ -30,127 +30,121 @@ class Variable(float):
         self.super.__init__()
 
     @operation_overload
-    def __add__(self, x): pass
-    @operation_overload
-    def __sub__(self, x): pass
-    @operation_overload
-    def __mul__(self, x): pass
-    # @operation_overload
-    # def __floordiv__(self, x): pass
-    @operation_overload
-    def __truediv__(self, x): pass
-    # @operation_overload
-    # def __mod__(self, x): pass
-    # @operation_overload
-    # def __divmod__(self, x): pass
-    @operation_overload
-    def __pow__(self, x): pass
-    @operation_overload
-    def __radd__(self, x): pass
-    @operation_overload
-    def __rsub__(self, x): pass
-    @operation_overload
-    def __rmul__(self, x): pass
-    # @operation_overload
-    # def __rfloordiv__(self, x): pass
-    @operation_overload
-    def __rtruediv__(self, x): pass
-    # @operation_overload
-    # def __rmod__(self, x): pass
-    # @operation_overload
-    # def __rdivmod__(self, x): pass
-    @operation_overload
-    def __rpow__(self, x): pass
-    
-    def __dadd__(self):
+    def __add__(self, x):
         # x is this variable
         # a, b are parent one and two
         # x = a + b
         # dx/da = 1, dx/db = 1
         return [lambda a, b: 1., lambda b, a: 1.]
 
-    def __dsub__(self):
-        # x is this variable
+    @operation_overload
+    def __sub__(self, x):
+         # x is this variable
         # a, b are parent one and two
         # x = a - b
         # dx/da = 1, dx/db = -1
         return [lambda a, b: 1., lambda a, b: -1.]
 
-    def __dmul__(self):
+    @operation_overload
+    def __mul__(self, x):
         # x is this variable
         # a, b are parent one and two
         # x = a * b
         # dx/da = b, dx/db = a
         return [lambda a, b: b , lambda a, b: a]
 
-    def __dtruediv__(self):
+    # @operation_overload
+    # def __floordiv__(self, x): pass
+
+    @operation_overload
+    def __truediv__(self, x):
         # x is this variable
         # a, b are parent one and two
         # x = a / b
         # dx/da = 1/b, dx/db = -a/b/b
         return [lambda a, b: 1/b, lambda a, b: -a/b/b]
 
-    def __dpow__(self):
+    # @operation_overload
+    # def __mod__(self, x): pass
+    # @operation_overload
+    # def __divmod__(self, x): pass
+
+    @operation_overload
+    def __pow__(self, x):
         # x is this variable
         # a, b are parent one and two
         # x = a ^ b
         # dx/da = b*a^(b-1), dx/db = a^b*ln(a)
         return [lambda a, b: b*a**(b-1), lambda a, b: a**(b)*log(b)]
 
-    def __dradd__(self):
-        # refer to __dadd__
-        return self.__dadd__()
+    @operation_overload
+    def __radd__(self, x):
+        # x is this variable
+        # a, b are parent one and two
+        # x = a + b
+        # dx/da = 1, dx/db = 1
+        return [lambda a, b: 1., lambda b, a: 1.]
 
-    def __drsub__(self):
+    @operation_overload
+    def __rsub__(self, x):
         # x is this variable
         # a, b are parent one and two
         # x = b - a
         # dx/da = -1, dx/db = 1
         return [lambda a, b: -1., lambda a, b: 1.]
 
-    def __drmul__(self):
-        # refer to __dmul__
-        return self.__dmul__()
+    @operation_overload
+    def __rmul__(self, x):
+        # x is this variable
+        # a, b are parent one and two
+        # x = a * b
+        # dx/da = b, dx/db = a
+        return [lambda a, b: b , lambda a, b: a]
 
-    def __drtruediv__(self):
+    # @operation_overload
+    # def __rfloordiv__(self, x): pass
+
+    @operation_overload
+    def __rtruediv__(self, x):
         # x is this variable
         # a, b are parent one and two
         # x = b / a
         # dx/da = -b/a/a, dx/db = 1/a
         return [lambda a, b: -b/a/a, lambda a, b: 1/a]
-    
-    def __drpow__(self):
+
+    # @operation_overload
+    # def __rmod__(self, x): pass
+    # @operation_overload
+    # def __rdivmod__(self, x): pass
+
+    @operation_overload
+    def __rpow__(self, x):
         # x is this variable
         # a, b are parent one and two
         # x = b ^ a
         # dx/da = b^a*ln(b), dx/db = a*b^(a-1)
         return [lambda a, b: b**(a)*log(b), lambda a, b: a*b**(a-1)]
-
+    
     @operation_overload
-    def __neg__(self): pass
-    @operation_overload
-    def __pos__(self): pass
-    @operation_overload
-    def __abs__(self): pass
-
-    def __dneg__(self):
+    def __neg__(self):
         # x is this variable
         # a is parent
         # x = -a
         # dx/da = -1
         return [lambda a: -1.]
-    
-    def __dpos__(self):
+
+    @operation_overload
+    def __pos__(self):
         # x is this variable
         # a is parent
         # x = +a
         # dx/da = 1
         return [lambda a: 1.]
 
-    def __dabs__(self):
+    @operation_overload
+    def __abs__(self):
         # x is this variable
         # a is parent
         # x = a
         # dx/da = 1 if a > 0 else -1
-        return [lambda a: 1. if a > 0 else -1.]
-    pass
+        return [lambda a: 1. if a > 0 else -1.]    
