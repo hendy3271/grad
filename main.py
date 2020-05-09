78c5a15cf0fdcbbbe8ff1c29ab92f29c380b592d
hendy3271 <paul.g.hendy@gmail.com>
2020-05-09 11:52:50 +1000

    more in main

--- main.py
+++ main.py
@@ -1,19 +1,12 @@
-from grad import grad
+import test
+from grad import elementwise_grad as grad
 from grad.math import sin, tanh, exp, pi
 from grad.vector import vectorize, linspace
 from matplotlib.pyplot import plot, show, legend
 
 x = list(linspace(-pi, pi, 100))
 
-@vectorize
-def square(x):
-    return x**2
-
-@vectorize
-def relu(x):
-    if x < 0.:
-        return 0.
-    return x
+relu = vectorize(lambda x: max(x, 0.))
 
 tanh, tanh_ = vectorize(tanh), tanh
 exp, exp_ = vectorize(exp), exp
@@ -32,11 +25,26 @@ show()
 
 @vectorize
 def f1(x):
-    return (x-pi)*(x+pi)/pi/pi
-    
+    y = 0.
+    m = 1.
+    for n in range(20):
+        m *= n if n > 0 else 1
+        y += x**n/m
+    return y
+
 y = f1(x)
 dydx = grad(f1)(x)
 ddydxdx = grad(grad(f1))(x)
+
+plot(x, y, label = 'y')
+plot(x, dydx, label = 'dydx')
+plot(x, ddydxdx, label = 'ddydxdx')
+legend()
+show()
+
+y = relu(x)
+dydx = grad(relu)(x)
+ddydxdx = grad(grad(relu))(x)
 
 plot(x, y, label = 'y')
 plot(x, dydx, label = 'dydx')
